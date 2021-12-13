577a32b3e60e761f36c59a4eab975093ec7edc1d
Shubham Girdhar <girdharshubham@hotmail.com>
2021-12-13 19:20:40 +0530

    Named additions for handlers added in client channel initializer

--- zio-http/src/main/scala/zhttp/service/client/ClientChannelInitializer.scala
+++ zio-http/src/main/scala/zhttp/service/client/ClientChannelInitializer.scala
@@ -3,6 +3,7 @@ package zhttp.service.client
 import io.netty.channel.{Channel, ChannelHandler, ChannelInitializer, ChannelPipeline}
 import io.netty.handler.codec.http.{HttpClientCodec, HttpObjectAggregator}
 import zhttp.service.client.ClientSSLHandler.ClientSSLOptions
+import zhttp.service.{HTTP_CLIENT_CODEC, OBJECT_AGGREGATOR}
 
 final case class ClientChannelInitializer[R](
   channelHandler: ChannelHandler,
@@ -12,8 +13,8 @@ final case class ClientChannelInitializer[R](
   override def initChannel(ch: Channel): Unit = {
     val p: ChannelPipeline = ch
       .pipeline()
-      .addLast(new HttpClientCodec)
-      .addLast(new HttpObjectAggregator(Int.MaxValue))
+      .addLast(HTTP_CLIENT_CODEC, new HttpClientCodec)
+      .addLast(OBJECT_AGGREGATOR, new HttpObjectAggregator(Int.MaxValue))
       .addLast(channelHandler)
 
     if (scheme == "https") {
